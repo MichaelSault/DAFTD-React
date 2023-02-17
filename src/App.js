88e967371b2e9fb4f7ab688d28070e969dfe3783
Michael Sault <michael_sault@hotmail.com>
2023-02-17 05:08:01 +0000

    mongodb atlas config

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Button from 'react-bootstrap/Button';
 
 import './App.css';
 
+//require("./database/database").connect()
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +12,7 @@ function App() {
         <p>
           Did You Feed The Dog?
         </p>
-        <Button size="lg">The Dog Has Been Fed</Button>
+        <Button size="lg">I Fed The Dog!</Button>
       </header>
     </div>
   );
