c37fe615f1c1e92b9c6b1ba6e8b2f5bfc45a66e8
MichaelSault <michael_sault@hotmail.com>
2023-03-03 21:54:01 -0500

    added adoption page front-end

--- dataDog/src/App.js
+++ dataDog/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import DogWalk from './component/dogwalk.component';
 import Login from './component/login.component';
 import SignUp from './component/signup.component';
+import Adopt from './component/dogAdoption.component';
 
 
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<DogWalk />}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/adopt' element={<Adopt/>}/>
       </Routes>
     </BrowserRouter>
     </>
